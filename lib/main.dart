0ce09610c586c7ced1a0dc587141b42436270795
isis simmons <isissimmons20@isiss-MBP.attlocal.net>
2024-07-22 22:33:54 -0400

    added signup and firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
-//import 'firebase_options.dart';
-//import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-//import '/login.dart';
 import '/splashScreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp(
-    //options: DefaultFirebaseOptions.currentPlatform,
-  //);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 

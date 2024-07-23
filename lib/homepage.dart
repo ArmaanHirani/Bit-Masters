7fad1e5962d283f10a8d56f7a8cd01ba7698cd81
ArmaanHirani <armaan.hirani17861@gmail.com>
2024-07-23 10:13:20 -0400

    changed around homepage to look better

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -13,83 +13,136 @@ class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.blue, Colors.yellow],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+      body: Row(
+        children: [
+Expanded(
+  flex: 2, // 20% of the screen width
+  child: Container(
+    color: Color.fromARGB(255, 62, 197, 204),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly, // evenly spaced
+      children: [
+        SizedBox(height: 20),
+        Text(
+          'Task Priority', // add this Text widget
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
           ),
         ),
-        child: Column(
-          children: [
-            SizedBox(height: 20),
-            Container(
-              alignment: Alignment.center,
-              child: Text('Homepage', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold, color: Colors.white)),
+        SizedBox(height: 20), // add some space between the text and the buttons
+        Container(
+          height: 60, 
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.zero, // make it a rectangle
+              ),
             ),
-            SizedBox(height: 10),
-            TextButton(
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero,
-                    side: BorderSide(width: 2, color: Colors.black),
-                  ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => TaskListPage(priority: 'High')),
+              );
+            },
+            child: Text('High'),
+          ),
+        ),
+        Container(
+          height: 60, 
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.zero, // make it a rectangle
+              ),
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => TaskListPage(priority: 'Medium')),
+              );
+            },
+            child: Text('Medium'),
+          ),
+        ),
+        Container(
+          height: 60, 
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.zero, // make it a rectangle
+              ),
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => TaskListPage(priority: 'Low')),
+              );
+            },
+            child: Text('Low'),
+          ),
+        ),
+        Container(
+          height: 60, 
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.zero, // make it a rectangle
+              ),
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => TaskListPage(priority: 'None')),
+              );
+            },
+            child: Text('All'),
+          ),
+        ),
+      ],
+    ),
+  ),
+),
+          Expanded(
+            flex: 8, // 80% of the screen width
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.blue, Colors.yellow],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
                 ),
               ),
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) => AddTaskDialog(),
-                );
-              },
-              child: Text('Add New Task', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black)),
+              child: Column(
+                children: [
+                  SizedBox(height: 20),
+                  Container(
+                    alignment: Alignment.center,
+                    child: Text('Home Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold, color: Colors.white)),
+                  ),
+                  SizedBox(height: 10),
+                  TextButton(
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero,
+                          side: BorderSide(width: 2, color: Colors.black),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AddTaskDialog(),
+                      );
+                    },
+                    child: Text('Add New Task', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black)),
+                  ),
+                ],
+              ),
             ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => TaskListPage(priority: 'High')),
-                    );
-                  },
-                  child: Text('High'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => TaskListPage(priority: 'Medium')),
-                    );
-                  },
-                  child: Text('Medium'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => TaskListPage(priority: 'Low')),
-                    );
-                  },
-                  child: Text('Low'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => TaskListPage(priority: 'None')),
-                    );
-                  },
-                  child: Text('None'),
-                ),
-              ],
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
